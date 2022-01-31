c783e818f6abc8219626f228d829504b1d288912
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-31 20:46:19 +0300

    Proje dosyası ekle.

---
+++ LINQTOOBJECT/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQTOOBJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Urunler = new List<Urun>();
+            Urun u = new Urun();
+            u.urunadi = "Yiyecek";
+            Kategori kat01 = new Kategori() { KategoriID = 1, KategoriAdi = "Meyveler" };
+            Kategori kat02 = new Kategori() { KategoriID = 2, KategoriAdi = "Sebzeler" };
+            Urunler.Add(new Urun()
+            {
+                urunID = 1,
+                urunadi = "Elma",
+                kategori = kat01,
+                fiyat = 3.45m,
+                stok = 250
+            });
+            Urunler.Add(new Urun()
+            {
+                urunID = 2,
+                urunadi = "Kiraz",
+                kategori = kat01,
+                fiyat = 2.30m,
+                stok = 300
+            });
+            Urunler.Add(new Urun()
+            {
+                urunID = 3,
+                urunadi = "Kabak",
+                kategori = kat02,
+                fiyat = 5.45m,
+                stok = 150
+            });
+            Urunler.Add(new Urun()
+            {
+                urunID = 4,
+                urunadi = "Patlıcan",
+                kategori = kat01,
+                fiyat = 1.50m,
+                stok = 500
+            });
+
+        }
+        List<Urun> Urunler;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Urun> Secilenler = new List<Urun>();
+            foreach (Urun item in Urunler)
+            {
+                if (item.fiyat>3)
+                {
+                    Secilenler.Add(item);
+                }
+            }
+            dataGridView1.DataSource = Secilenler;
+        }
+    }
+}
---
+++ LINQTOOBJECT/Urun.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQTOOBJECT
+{
+    public class Urun
+    {
+        public int urunID { get; set; }
+        public string urunadi { get; set; }
+        public Kategori kategori { get; set; }
+        public decimal fiyat { get; set; }
+        public int stok { get; set; }
+        public override string ToString()
+        {
+            return urunID + " " + urunadi;
+        }
+    }
+    public class Kategori
+    {
+        public int KategoriID { get; set; }
+        public string KategoriAdi { get; set; }
+        public override string ToString()
+        {
+            return KategoriAdi;
+        }
+
+
+    }
+}
